72131bf7264cdbd3c8ec5f130dec59ad69720c80
Pramod Dange <123957464+Pramoddange@users.noreply.github.com>
2024-06-06 21:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ // Get the width and height of the window
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    // Get the div with id sizeInfo
+    const sizeInfoDiv = document.getElementById('sizeInfo');
+
+    // Update the content of the h1 tag within the sizeInfo div
+    sizeInfoDiv.innerHTML = `<h1>Width: ${width} and Height: ${height}</h1>`;
